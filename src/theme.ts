920cf6a33a0aee5ea607d90d3a835820ace31418
chominho14 <chominho14@naver.com>
2022-02-23 00:48:33 +0900

    v0.0.1 (TodoList - 2 - Basic Form Setting)

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#CCB9BC",
+  textColor: "#FFEBEE",
+  accentColor: "#A09698",
+  hoverColor: "black",
+};
